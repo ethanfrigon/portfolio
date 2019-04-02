5d280ff5184391f1b504698f9101bbdddd19dca3
Ethan Frigon <ethan.frigon@gmail.com>
2019-04-01 19:16:39 -0700

    delete stray div

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   $(document).scroll(function() { 
     scroll_start = $(this).scrollTop();
     if(scroll_start > offset.top) {
-      $('#navbar').css('background-color', '#306DA7');
+      $('#navbar').css('background-color', '#2E66A1');
     } else {
       $('#navbar').css('background-color', 'transparent');
     }
